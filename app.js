4e97094a41589b67eece873a98f723b68d234271
Marquis Parks <mparks@Marquiss-MacBook-Pro.local>
2017-05-10 14:00:16 -0700

    first push

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-import routes from './index.js'
-import ejs from 'ejs'
-import path from 'path'
-import logger from 'morgan'
-import bodyParser from 'body-parser'
-import express from 'express'
+const routes = ('./server.js')
+const ejs = 'ejs'
+const path = ('path')
+const logger = ('morgan')
+const bodyParser = ('body-parser')
+const express = require ('express')
 const server = express()
 
 server.set('views', path.join(__dirname, 'views'))
@@ -17,3 +17,4 @@ server.use('/', routes)
 
 server.listen(process.env.PORT || 8080)
 module.exports = server
+
